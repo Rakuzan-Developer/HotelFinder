29bafeb55c8239b277a5e082cc64b2d2d4762bee
Rakuzan-Developer <190758+Rakuzan-Developer@users.noreply.github.com>
2020-08-20 10:44:35 +0300

    Hotel Finder

---
+++ HotelFinder/HotelFinder.DataAccess/HotelDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HotelFinder.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelFinder.DataAccess
+{
+    public class HotelDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server:LAPTOP-657RCKM3; Database:HotelDb; uid:sas; pwd:1234");
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
